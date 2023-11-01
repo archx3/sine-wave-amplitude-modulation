c38415c9d1987bd25194bda74e83e0a45e276d3b
Kobina_Koomson <georgeranch31@gmail.com>
2023-11-01 18:26:34 +0000

    feat: Add createElement function, simplify js/main.js
    This commit introduces a more streamlined version of the js/main.js file. It adds a new 'createElement' function for dynamic DOM manipulation and eliminates verbose code sections. The settings for the vertical divisions were also encapsulated inside the SETTINGS object for easier configuration.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-const PAPER = document.getElementById('paper');
-const PEN = PAPER.getContext('2d');
+function createElement(tag, props, ...children) {
+  const ELEMENT = Object.assign(document.createElement(tag), props);
+
+  for (let child of children) {
+    if (typeof child === "string") {
+      ELEMENT.appendChild(document.createTextNode(child));
+    } else {
+      ELEMENT.appendChild(child);
+    }
+  }
+
+  if (props.data) {
+    for (key in props.data) {
+      ELEMENT.dataset[key] = props.data[key];
+    }
+  }
+
+  for (key in props.style) {
+    ELEMENT.style[key] = props.style[key]
+  }
+
+  return ELEMENT;
+}
 
 const colors = [
   "#8D83EF", // 0
@@ -25,137 +46,9 @@ const colors = [
   "#FEDCD1"
 ];
 
-function clamp (value, min = 0, max = 1.0) {
-  return Math.min(Math.max(value, min), max);
+const SETTINGS = {
+  VERTICAL_DIVISIONS: 21,
 }
-
-// const GRG_IN_SF = document.getElementById('grg-in-sf');
-
-// const { width : IMAGE_WIDTH, height : IMAGE_HEIGHT } = GRG_IN_SF;
-const WIDTH = 800;
-const HEIGHT = 800;
-
-const VERTICAL_DIVISIONS = 15;
-const VERTICAL_DIVISION_HEIGHT = HEIGHT / VERTICAL_DIVISIONS ;
-const FREQUENCY = 100;
-const MAX_AMPLITUDE = VERTICAL_DIVISION_HEIGHT / 3;
-
-let time = 0; // we need to track our animation over time the frames
-const PHASE = time / 5; // or frameCount / 30, i.e. 30 frames per second
-
-// region creating gradient
-const GRADIENT = PEN.createLinearGradient(0, 0, WIDTH, 0);
-GRADIENT.addColorStop(0, colors[0]);
-GRADIENT.addColorStop(1, colors[7]);
-
-const NUMBER_OF_STOPS = 10;
-for (let i = 0; i < NUMBER_OF_STOPS; i++) {
-  GRADIENT.addColorStop(clamp(10 / (NUMBER_OF_STOPS * i)), colors[i]);
-}
-
-// endregion
-
-function initializeCanvas (image) {
-  const width = PAPER.clientWidth,
-    height= PAPER.clientHeight;
-
-  setCanvasSize(PAPER, width, height);
-  paintBackground(PEN, 255, 255, 255, 1, width, height);
-  stroke(PEN, 0, 0, 0, 1);
-}
-
-function createShadowCanvasFromImage (image) {
-  const { width, height } = image;
-
-  const PIXEL_CANVAS = document.createElement('canvas');
-  PIXEL_CANVAS.width = width;
-  PIXEL_CANVAS.height = height;
-  const PIXEL_BRUSH = PIXEL_CANVAS.getContext('2d');
-
-  PIXEL_BRUSH.drawImage(image, 0, 0, width, height);
-  const IMAGE_DATA = PIXEL_BRUSH.getImageData(0, 0, width, height);
-  const PIXELS = IMAGE_DATA.data; // Uint8ClampedArray have a range of 0-255; just like rgba values
-
-  const COMPRESSED_PIXELS = Array.from({ length : width * height }, (_, i) => {
-    const pixelIndex = i * 4;
-
-    const r = PIXELS[pixelIndex];
-    const g = PIXELS[pixelIndex + 1];
-    const b = PIXELS[pixelIndex + 2];
-
-    // this averages the rgb values which is more like the grayscale value of the color but no necessarily the grayscale value the human eye perceives
-    return (r + g + b) / 3;
-  });
-
-  return { PIXEL_CANVAS, PIXEL_BRUSH, IMAGE_DATA, PIXELS, COMPRESSED_PIXELS };
-}
-
-
-function drawWave (firstPoint, y) {
-  let previousPoint = firstPoint;
-  // Object.assign(previousPoint, firstPoint);
-  const PHASE = time / 5; // or frameCount / 30, i.e. 30 frames per second
-  // we're going to draw a sine wave as long as the canvas is wide
-  for (let x = 0; x < WIDTH; x++) {
-    const ANGLE = getValueFromRangeMapping(x, [0, WIDTH], [0, Math.PI * 2]);
-    const SIN_VALUE = Math.sin(PHASE + ANGLE * FREQUENCY); // adding a phase shift to the sine wave
-
-    const grayScaleIndex = Math.floor(y) * WIDTH + x
-
-    // console.log( grayScaleIndex, )
-    const AMPLITUDE = getValueFromRangeMapping(grayScaleIndex, [0, 255], [ 0, MAX_AMPLITUDE,]);
-    // const _AMPLITUDE = mapRange(grayScaleIndex, 0, 255, MAX_AMPLITUDE, 0);
-    const MODULATED_AMPLITUDE = getValueFromRangeMapping(Math.sin(ANGLE * FREQUENCY / 7), [-1, 1], [0, MAX_AMPLITUDE]);
-
-    console.log({ grayScaleIndex, AMPLITUDE, MODULATED_AMPLITUDE })
-    // const MODULATED_AMPLITUDE = 0;
-    const point = { x, y : y + SIN_VALUE * MODULATED_AMPLITUDE};
-
-    drawLine(PEN, previousPoint, point);
-    previousPoint = point;
-
-  }
-}
-
-
-const draw = function draw () {
-  paintBackground(PEN, 255, 255, 255, 1, WIDTH, HEIGHT);
-
-  for (let div = 0; div < VERTICAL_DIVISIONS; div++) {
-    const y = (VERTICAL_DIVISION_HEIGHT / 2) + div * VERTICAL_DIVISION_HEIGHT;
-    // console.log({ y })
-    drawWave({ x: -1, y }, y);
-  }
-
-  time++;
-  // requestAnimationFrame(draw);
-}
-
-function init () {
-
-  const IMAGE = new Image();
-  IMAGE.src = 'img/grg-in-osu.jpg';
-
-  IMAGE.onload = () => {
-    const {
-      PIXEL_CANVAS,
-      PIXEL_BRUSH,
-      IMAGE_DATA,
-      PIXELS,
-      COMPRESSED_PIXELS
-    } = createShadowCanvasFromImage(IMAGE);
-
-    initializeCanvas({ width : WIDTH, height : HEIGHT });
-
-    draw();
-  }
-}
-
-document.addEventListener('readystatechange', () => {
-  if (document.readyState === 'complete') {
-    init();
-  }
-});
 
 
 
