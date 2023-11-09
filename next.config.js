bd6b796465b22f061a843d5358a5864049b2bb63
Kobina_Koomson <georgeranch31@gmail.com>
2023-11-09 23:17:07 +0000

    feat: Add support for SVG in Next.js using next-react-svg
    This commit integrates the next-react-svg package into the Next.js configuration. This allows for direct importing and usage of SVG files in the React components. Typescript configuration and declarations are also updated to recognize these imported SVG files.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path');
+
+/**
+ * @type {import('next-react-svg').NextReactSvgConfig}
+ */
+const nextReactSvgConfig = {
+  include: path.resolve(__dirname, 'src/assets/svg'),
+};
+
+const withReactSvg = require('next-react-svg')(nextReactSvgConfig);
+
+module.exports = withReactSvg(nextConfig);
