8e69972bfee883a17525ee87cf58e34179a77e23
Kobina_Koomson <georgeranch31@gmail.com>
2023-11-12 23:26:27 +0000

    feat: Add colorPallets configuration file
    A new configuration file, color-pallets.js, has been added to the src/config directory. This file exports a colorPallets constant that contains arrays of color strings for different color-schemes: DEFAULT, VINTAGE_GREEN, SWEET_ORANGE, CAMIDOH_BLUES, and KWESI_ARTHUR.

---
+++ src/config/color-pallets.js
@@ -0,0 +1,72 @@
+/**
+ * Color Pallets
+ * @type {{CAMIDOH_BLUES: string[], KWESI_ARTHUR: string[], VINTAGE_GREEN: string[], DEFAULT: string[], SWEET_ORANGE: string[]}}
+ */
+export const colorPallets = {
+  DEFAULT: [
+    "#8D83EF", // 0
+    "#AE69F0", // 1
+    "#D46FF1",
+    "#DB5AE7", // 3
+    "#D911DA",
+    "#D601CB", // 5
+    "#E713BF",
+    "#F24CAE", // 7
+    "#FB79AB",
+    "#FFB6C1", // 9
+    "#D0E7F5",
+    "#D9E7F4",
+    "#D6E3F4",
+    "#BCDFF5",
+    "#B7D9F4",
+    "#C3D4F0",
+    "#9DC1F3",
+    "#9AA9F4",
+    "#FED2CF",
+    "#FDDFD5",
+    "#FEDCD1"
+  ],
+  VINTAGE_GREEN: [
+    "#FFFFDD",
+    "#D5E8D4",
+    "#B7D3B3",
+    "#9DBF95",
+    "#A2C579",
+    "#fffb00",
+    "#7CAE7A",
+    "#5F9E60",
+    "#4B8B4E",
+    "#D2DE32",
+    "#3A7A3E",
+    "#2D6930",
+    "#37d005",
+    "#1F5823",
+    "#14481A",
+    "#016A70"
+  ],
+  SWEET_ORANGE: [
+    "#2B3499",
+    "#FF6C22",
+    "#FF9209",
+    "#FFD099",
+    "#FF5B22"
+  ],
+  CAMIDOH_BLUES: [
+    "#0C356A",
+    "#0174BE",
+    "#FFC436",
+    "#FFF0CE",
+  ],
+  KWESI_ARTHUR: [
+    "#0802A3",
+    "#FF4B91",
+    "#FF7676",
+    "#FFCD4B",
+    "#2AD705",  // Vibrant Green
+    "#1446A0",  // Deep Blue
+    "#AB05F2",  // Vivid Purple
+    "#F23005",  // Bright Red
+    "#05A4F2",  // Bright Blue
+    "#F2A005",  // Vibrant Orange
+  ],
+}
